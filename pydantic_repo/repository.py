ad47d5fc51d55d237be09f719d588e3fd47f2c90
koalth <koalrube@gmail.com>
2024-09-03 20:09:37 -0400

    made generic service

--- pydantic_repo/repository.py
+++ pydantic_repo/repository.py
@@ -15,7 +15,7 @@ class GenericRepository(Generic[ModelType, CreateSchemaType, UpdateSchemaType, E
     def __init__(self, model: Type[ModelType], entity: Type[EntityType]):
         self.model = model
         self.entity = entity
-        
+
     async def create(self, obj_in: CreateSchemaType) -> EntityType:
         db_obj = self.model(**obj_in.model_dump())
         async with sessionmanager.session() as session:
@@ -23,14 +23,14 @@ class GenericRepository(Generic[ModelType, CreateSchemaType, UpdateSchemaType, E
             await session.commit()
             await session.refresh(db_obj)
             return self.entity.model_validate(db_obj)
-        
+
     async def get(self, id: uuid.UUID) -> EntityType:
         stmt = select(self.model).where(self.model.id == id)
         async with sessionmanager.session() as session:
             results = await session.execute(stmt)
             obj = results.scalar_one()
             return self.entity.model_validate(obj)
-        
+
     async def get_all(self, *filter_conditions) -> List[EntityType]:
         stmt = select(self.model)
         if filter_conditions:
@@ -38,7 +38,7 @@ class GenericRepository(Generic[ModelType, CreateSchemaType, UpdateSchemaType, E
         async with sessionmanager.session() as session:
             results = await session.execute(stmt)
             return [self.entity.model_validate(obj) for obj in results.scalars().all()]
-        
+
     async def update(self, id: uuid.UUID, obj_in: UpdateSchemaType) -> EntityType:
         stmt = select(self.model).where(self.model.id == id)
         async with sessionmanager.session() as session:
@@ -50,13 +50,33 @@ class GenericRepository(Generic[ModelType, CreateSchemaType, UpdateSchemaType, E
             await session.commit()
             await session.refresh(db_obj)
             return self.entity.model_validate(db_obj)
-        
+
     async def delete(self, id: uuid.UUID) -> bool:
         stmt = delete(self.model).where(self.model.id == id)
         async with sessionmanager.session() as session:
             results = await session.execute(stmt)
             await session.commit()
             return results.rowcount > 0
-        
-        
-        
+
+
+class GenericService(
+    Generic[ModelType, CreateSchemaType, UpdateSchemaType, EntityType]
+):
+    repository: GenericRepository
+
+    async def create(self, create_entity: CreateSchemaType) -> EntityType:
+        return await self.repository.create(create_entity)
+
+    async def get(self, id: uuid.UUID) -> EntityType:
+        return await self.repository.get(id)
+
+    async def get_all(self, *filter_conditions) -> List[EntityType]:
+        return await self.repository.get_all(*filter_conditions)
+
+    async def update(
+        self, id: uuid.UUID, update_entity: UpdateSchemaType
+    ) -> EntityType:
+        return await self.repository.update(id, update_entity)
+
+    async def delete(self, id: uuid.UUID) -> bool:
+        return await self.repository.delete(id)
--- pydantic_repo/service.py
+++ pydantic_repo/service.py
@@ -1,31 +1,14 @@
-from .repository import GenericRepository
+from .repository import GenericRepository, GenericService
 from .model import ItemOrm
 from .entity import Item, ItemCreate, ItemUpdate
 from uuid import UUID
-from typing import List
+from typing import List, Generic, TypeVar, Type
+
 
 class ItemRepository(GenericRepository[ItemOrm, ItemCreate, ItemUpdate, Item]):
     def __init__(self):
         super().__init__(ItemOrm, Item)
-        
-class ItemService:
-    
-    itemRepository: ItemRepository = ItemRepository()
-    
-    async def create_item(self, item: ItemCreate) -> Item:
-        return await self.itemRepository.create(item)
-    
-    async def get_item(self, id: UUID) -> Item:
-        return await self.itemRepository.get(id)
-    
-    async def get_items_by_price_range(self, min_price: float, max_price: float) -> List[Item]:
-        return await self.itemRepository.get_all(
-            ItemOrm.price >= min_price,
-            ItemOrm.price <= max_price
-        )
-    
-    async def update_item(self, id: UUID, item_update: ItemUpdate) -> Item:
-        return await self.itemRepository.update(id, item_update)
-    
-    async def delete_item(self, id: UUID) -> None:
-        await self.itemRepository.delete(id)
+
+
+class ItemService(GenericService[ItemOrm, ItemCreate, ItemUpdate, Item]):
+    repository = ItemRepository()
